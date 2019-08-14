4adee91b98a972416f62fe5c4caaf7f828f4310e
Nicolas Barray <nbarray@gmail.com>
2019-08-14 10:44:55 +0200

    feat(colors): add missing alphas + foreground colors

--- src/constants/palette.js
+++ src/constants/palette.js
@@ -1,22 +1,49 @@
-export const white = '#ffffff'
-export const black = '#000000'
+function hexToRGBA(hex, alpha) {
+  const r = parseInt(hex.slice(1, 3), 16),
+    g = parseInt(hex.slice(3, 5), 16),
+    b = parseInt(hex.slice(5, 7), 16)
 
+  if (alpha) {
+    return 'rgba(' + r + ', ' + g + ', ' + b + ', ' + alpha + ')'
+  } else {
+    return 'rgb(' + r + ', ' + g + ', ' + b + ')'
+  }
+}
+
+export const white = '#ffffff'
+export const whiteAlpha = alpha => hexToRGBA(white, alpha)
 export const lightGrey = '#dddddd'
-export const lightGreyAlpha = alpha => `rgba(221, 221, 221, ${alpha})`
+export const lightGreyAlpha = alpha => hexToRGBA(lightGrey, alpha)
 export const mediumGrey = '#708c91'
+export const mediumGreyAlpha = alpha => hexToRGBA(mediumGrey, alpha)
 export const darkGrey = '#054752'
-export const darkGreyAlpha = alpha => `rgba(5, 71, 82, ${alpha})`
-export const darkGreyShade = 'rgba(5, 71, 82, 0.2)'
+export const darkGreyAlpha = alpha => hexToRGBA(darkGrey, alpha)
+export const darkGreyShade = darkGreyAlpha(0.2)
+export const black = '#000000'
+export const blackAlpha = alpha => hexToRGBA(black, alpha)
 
 export const berryBlue = '#00aff5'
-export const berryBlueAlpha = alpha => `rgba(0, 175, 245, ${alpha})`
+export const berryBlueAlpha = alpha => hexToRGBA(berryBlue, alpha)
 export const lettuceGreen = '#5dd167'
+export const lettuceGreenAlpha = alpha => hexToRGBA(lettuceGreen, alpha)
 export const beetrootPurple = '#55468c'
+export const beetrootPurpleAlpha = alpha => hexToRGBA(beetrootPurple, alpha)
 export const lightBeetrootPurple = '#5E48AF'
-export const lightBeetrootPurpleAlpha = alpha => `rgba(94, 72, 175, ${alpha})`
+export const lightBeetrootPurpleAlpha = alpha => hexToRGBA(lightBeetrootPurple, alpha)
 export const radishRed = '#f53f5b'
+export const radishRedAlpha = alpha => hexToRGBA(radishRed, alpha)
 export const potatoYellow = '#e9e163'
+export const potatoYellowAlpha = alpha => hexToRGBA(potatoYellow, alpha)
+export const grapeYellow = '#625d11'
+export const grapeYellowAlpha = alpha => hexToRGBA(grapeYellow, alpha)
+export const grapeRed = '#a61b30'
+export const grapeRedAlpha = alpha => hexToRGBA(grapeRed, alpha)
+export const grapeGreen = '#0c7615'
+export const grapeGreenAlpha = alpha => hexToRGBA(grapeGreen, alpha)
 
 export const warning = potatoYellow
+export const warningText = grapeYellow
 export const error = radishRed
+export const errorText = grapeRed
 export const success = lettuceGreen
+export const successText = grapeGreen
--- src/constants/palette.stories.js
+++ src/constants/palette.stories.js
@@ -7,7 +7,7 @@ import {CopyToClipboard} from 'react-copy-to-clipboard'
 import Showcase from '../stories/showcase'
 
 import {initializeQueue} from '../components/toast'
-import {palette, Toast, toastSuccess} from '../index'
+import {palette, Toast, toastSuccess, Info} from '../index'
 const ColorHex = styled.div`
   text-transform: lowercase;
 `
@@ -23,10 +23,15 @@ function Color({children, color, label, invert}) {
       <Showcase
         onClick={() => toastSuccess(`"${color}" in your clipboard !`)}
         variant="squared"
-        style={{backgroundColor: color, color: invert ? palette.darkGrey : '#fff', cursor: 'pointer'}}
+        style={{
+          backgroundColor: color,
+          color: invert ? palette.darkGrey : '#fff',
+          cursor: 'pointer',
+          justifyContent: 'space-around'
+        }}
       >
-        <ColorHex>{label || color}</ColorHex>
         <ColorName>{children}</ColorName>
+        <ColorHex>{label || color}</ColorHex>
       </Showcase>
     </CopyToClipboard>
   )
@@ -57,6 +62,9 @@ storiesOf('Palette', module)
   .add('Library', () => {
     return (
       <>
+        <Info>
+          <Info.Label label="White to black gradients" />
+        </Info>
         <Palette>
           <Color color={palette.white} invert>
             White
@@ -64,29 +72,69 @@ storiesOf('Palette', module)
           <Color color={palette.lightGrey} invert>
             Light grey
           </Color>
-          <Color label={'Alpha 20%'} color={palette.darkGreyShade} invert>
-            Dark grey shade
-          </Color>
           <Color color={palette.mediumGrey}>Medium grey</Color>
           <Color color={palette.darkGrey}>Dark grey</Color>
           <Color color={palette.black}>Black</Color>
         </Palette>
+
+        <Info>
+          <Info.Label label="White to black gradients (alpha)" />
+        </Info>
         <Palette>
+          <Color color={palette.whiteAlpha(0.6)} invert>
+            White
+          </Color>
+          <Color color={palette.lightGreyAlpha(0.6)} invert>
+            Light grey
+          </Color>
+          <Color color={palette.mediumGreyAlpha(0.6)}>Medium grey</Color>
+          <Color color={palette.darkGreyAlpha(0.6)}>Dark grey</Color>
+          <Color color={palette.blackAlpha(0.6)}>Black</Color>
+        </Palette>
+
+        <Info>
+          <Info.Label label="Color palette" />
+        </Info>
+        <Palette>
+          <Color color={palette.grapeGreen}>Grape Green</Color>
           <Color color={palette.lettuceGreen}>Lettuce Green</Color>
           <Color color={palette.berryBlue}>Berry Blue</Color>
           <Color color={palette.lightBeetrootPurple}>Light Beetroot Purple</Color>
           <Color color={palette.beetrootPurple}>Beetroot Purple</Color>
+          <Color color={palette.grapeRed}>Grape Red</Color>
           <Color color={palette.radishRed}>Radish Red</Color>
+          <Color color={palette.grapeYellow}>Grape Yellow</Color>
           <Color color={palette.potatoYellow} invert>
             Potato Yellow
           </Color>
         </Palette>
+
+        <Info>
+          <Info.Label label="Color palette (alpha)" />
+        </Info>
+        <Palette>
+          <Color color={palette.lettuceGreenAlpha(0.6)}>Lettuce Green</Color>
+          <Color color={palette.berryBlueAlpha(0.6)}>Berry Blue</Color>
+          <Color color={palette.lightBeetrootPurpleAlpha(0.6)}>Light Beetroot Purple</Color>
+          <Color color={palette.beetrootPurpleAlpha(0.6)}>Beetroot Purple</Color>
+          <Color color={palette.radishRedAlpha(0.6)}>Radish Red</Color>
+          <Color color={palette.potatoYellowAlpha(0.6)} invert>
+            Potato Yellow
+          </Color>
+        </Palette>
+
+        <Info>
+          <Info.Label label="Semantic colors (background & forground)" />
+        </Info>
         <Palette>
           <Color color={palette.success}>Success</Color>
+          <Color color={palette.successText}>Success Text</Color>
           <Color color={palette.warning} invert>
             Warning
           </Color>
+          <Color color={palette.warningText}>Warning Text</Color>
           <Color color={palette.error}>Error</Color>
+          <Color color={palette.errorText}>Error Text</Color>
         </Palette>
       </>
     )
