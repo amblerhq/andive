e3b4b496c9e764eb63a1f95af088665ed0499178
Nicolas Barray <nbarray@gmail.com>
2019-08-16 14:34:22 +0200

    fix(icon): fix typo in question icon import

--- src/components/icons/question.js
+++ src/components/icons/question.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
-import Question from '../../../img/icons/Question.svg'
+import Question from '../../../img/icons/question.svg'
 import Icon from '../icon'
 
 const QuestionOverride = styled(Question)`
