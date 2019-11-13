827e4bcd0a95b26920bc6a0c81752fef66c7fe12
lucamerzi <luca.merzetti@gmail.com>
2019-11-13 15:20:38 +0100

    Add import space

--- packages/andive/src/components/card.js
+++ packages/andive/src/components/card.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled, {css} from 'styled-components'
 import PropTypes from 'prop-types'
+
 import * as palette from '../constants/palette'
 
 const rootCss = css`
