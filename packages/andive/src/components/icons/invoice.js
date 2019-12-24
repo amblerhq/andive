98878d81041adeb7afcc0881325de2213c8e1b5c
smehdii <siddik.mehdi@gmail.com>
2019-12-24 15:50:40 +0100

    review changes

--- packages/andive/src/components/icons/invoice.js
+++ packages/andive/src/components/icons/invoice.js
@@ -1,17 +1,28 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
 import Invoice from '../../../img/icons/invoice.svg'
 import Icon from '../icon'
 
+const InvoiceOverride = styled(Invoice)`
+  & > g > text {
+    fill: ${props => props.color};
+  }
+`
+
 const InvoiceIcon = props => {
   return (
     <Icon {...props}>
-      <Invoice />
+      <InvoiceOverride color={props.color} />
     </Icon>
   )
 }
 
-InvoiceIcon.size = 40
+InvoiceIcon.propTypes = {
+  color: PropTypes.string
+}
+InvoiceOverride.size = 40
 
 export default InvoiceIcon
 export const InvoiceIconSvg = InvoiceIcon
