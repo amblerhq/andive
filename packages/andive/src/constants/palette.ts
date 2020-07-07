069155a64c79a5478ad25e3938ed9818b8a7d2cc
Nicolas Barray <nbarray@gmail.com>
2020-07-07 02:28:16 +0200

    chore(ts): export ts declarations

--- packages/andive/src/constants/palette.ts
+++ packages/andive/src/constants/palette.ts
@@ -43,7 +43,7 @@ export const success = mediumLettuceGreen
 export const successText = darkLettuceGreen
 export const border = mediumGrey
 
-export function hexToRGBA(hex, alpha) {
+export function hexToRGBA(hex: string, alpha: number) {
   const r = parseInt(hex.slice(1, 3), 16),
     g = parseInt(hex.slice(3, 5), 16),
     b = parseInt(hex.slice(5, 7), 16)
