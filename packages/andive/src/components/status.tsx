93d99e169aa4e83e5ffa2a9131ebd5bee5631cd8
Nicolas Barray <nbarray@gmail.com>
2020-08-19 17:38:04 +0200

    refactor(status): fix prop types and naming

--- packages/andive/src/components/status.tsx
+++ packages/andive/src/components/status.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
 import { Body3 } from './typography'
@@ -19,29 +18,24 @@ const StatusWrapper = styled(({ color, backgroundColor, ...props }) => (
   ${({ backgroundColor }) => backgroundColor && `background: ${backgroundColor};`}
 `
 
-const Status = styled(({ color, ...props }) => <Body3 {...props} />)`
-  ${({ color }) => color && `color: ${color};`}
-`
-
-export default function StatusComponent({
+interface StatusProps {
+  primary: string,
+  className?: string,
+  color?: string,
+  backgroundColor?: string
+  secondary?: string
+}
+export function Status({
+  className,
   color,
   backgroundColor,
   primary,
-  secondary,
-  ...props
-}) {
+  secondary
+}: StatusProps) {
   return (
-    <StatusWrapper backgroundColor={backgroundColor} {...props}>
-      {primary && <Status color={color}>{primary}</Status>}
-      {secondary && <Status color={color}>{secondary}</Status>}
+    <StatusWrapper className={className} color={color} backgroundColor={backgroundColor}>
+      {primary && <Body3 color={color}>{primary}</Body3>}
+      {secondary && <Body3 color={color}>{secondary}</Body3>}
     </StatusWrapper>
   )
-}
-
-StatusComponent.propTypes = {
-  status: PropTypes.string,
-  color: PropTypes.string,
-  backgroundColor: PropTypes.string,
-  primary: PropTypes.string,
-  secondary: PropTypes.string,
 }
