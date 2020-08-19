5abfb797e7648db92a06f906f8e142ed5f870665
Thomas <thomas.josso@gmail.com>
2020-08-19 17:46:17 +0200

    feat(status): add italic option to secondary

--- packages/andive/src/components/status.tsx
+++ packages/andive/src/components/status.tsx
@@ -18,24 +18,30 @@ const StatusWrapper = styled(({ color, backgroundColor, ...props }) => (
   ${({ backgroundColor }) => backgroundColor && `background: ${backgroundColor};`}
 `
 
+const SecondaryStatus = styled(({secondayItalic, ...props}) => (<Body3 {...props} />))`
+  font-style: ${props => (props.secondaryItalic ? 'italic' : 'none')};
+`
+
 interface StatusProps {
   primary: string,
   className?: string,
   color?: string,
   backgroundColor?: string
   secondary?: string
+  secondaryItalic?: boolean
 }
 export function Status({
   className,
   color,
   backgroundColor,
   primary,
-  secondary
+  secondary,
+  secondaryItalic = false
 }: StatusProps) {
   return (
     <StatusWrapper className={className} color={color} backgroundColor={backgroundColor}>
       {primary && <Body3 color={color}>{primary}</Body3>}
-      {secondary && <Body3 color={color}>{secondary}</Body3>}
+      {secondary && <SecondaryStatus color={color} secondaryItalic={secondaryItalic}>{secondary}</SecondaryStatus>}
     </StatusWrapper>
   )
 }
