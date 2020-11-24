62054259c8dda4f90789c14899f5e88e576d3db5
Luca <luca.merzetti@gmail.com>
2020-11-24 14:25:53 +0100

    feat(filter-button): update filter button border radius (#58)

--- packages/andive/src/components/button.tsx
+++ packages/andive/src/components/button.tsx
@@ -220,17 +220,16 @@ const FilterButton = styled(ResetButton)<{
   mobile?: boolean
   backgroundColor?: string
 }>`
-  border-radius: 20px;
+  border-radius: 16px;
   border: 1px solid ${props => (props.invert ? palette.darkPrimary : 'transparent')};
   height: ${props => (props.mobile ? 32 : 40)}px;
   background: ${({invert, backgroundColor}) => backgroundColor || (invert ? palette.white : palette.mediumBerryBlue)};
+  padding: ${props => (props.mobile ? '6px' : '8px')};
 `
-
 const FilterText = styled(({mobile, color, ...props}) => <div {...props} />)<{mobile?: boolean; color: string}>`
   ${props => (props.mobile ? body3Css : body1Css)};
-
   color: ${props => props.color};
-  padding: ${props => (props.mobile ? '6px 18px' : '8px 24px')};
+  padding: 0 ${props => (props.mobile ? '6px' : '8px')};
 `
 
 const FlatText = styled.div<{mobile?: boolean; color: string}>`
@@ -338,9 +337,11 @@ const Button = React.forwardRef(function Button(
   const contentJsx = (
     <>
       {leftIcon && React.cloneElement(leftIcon, {color})}
-      <ButtonLabel style={textStyle(leftIcon, rightIcon, label)} invert={invert} color={color} mobile={mobile}>
-        {label}
-      </ButtonLabel>
+      {label && (
+        <ButtonLabel style={textStyle(leftIcon, rightIcon, label)} invert={invert} color={color} mobile={mobile}>
+          {label}
+        </ButtonLabel>
+      )}
       {rightIcon && React.cloneElement(rightIcon, {color})}
     </>
   )
