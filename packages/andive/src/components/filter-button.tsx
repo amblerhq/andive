ef15d5e63ac42b004300ec8855067dbf1af2ff46
lucamerzi <luca.merzetti@gmail.com>
2020-11-25 22:06:33 +0100

    First commit

---
+++ packages/andive/src/components/filter-button.tsx
@@ -0,0 +1,66 @@
+import React, {DOMAttributes} from 'react'
+import styled, {css} from 'styled-components'
+
+import {body1Css, body3Css} from './typography'
+import * as palette from '../constants/palette'
+import FilterIcon from './icons/filter'
+
+const FilterButtonRoot = styled(({active, ...props}) => <button {...props} />)<{
+  active?: boolean
+}>`
+  background: ${({active, backgroundColor}) =>
+    backgroundColor || (active ? palette.mediumBerryBlue : palette.mediumGrey)};
+  border: 1px solid tomato;
+  border-radius: 16px;
+  cursor: pointer;
+  height: 40px;
+  padding: 8px;
+
+  display: flex;
+  align-items: center;
+
+  ${props =>
+    props.active &&
+    css`
+      *[data-andive-type='typography'],
+      svg {
+        color: ${palette.white};
+      }
+    `}
+
+  &:hover {
+    background-color: ${({active}) => (active ? palette.darkBerryBlue : palette.darkGrey)};
+  }
+`
+
+const FilterText = styled(({active, color, mobile, ...props}) => <div {...props} />)<{
+  active?: boolean
+  color?: string
+  mobile?: boolean
+}>`
+  ${props => (props.mobile ? body3Css : body1Css)};
+  color: ${({active, color}) => color || (active ? palette.white : palette.secondaryText)};
+  padding: 0 8px;
+`
+
+interface FilterButtonProps {
+  className?: string
+  label?: string | JSX.Element
+  onClick?: DOMAttributes<HTMLButtonElement>['onClick']
+  color?: string
+  active?: boolean
+  mobile?: boolean
+}
+
+export const FilterButton = ({active, color, onClick, className, label, mobile, ...buttonProps}: FilterButtonProps) => {
+  return (
+    <FilterButtonRoot onClick={onClick} className={className} active={active} {...buttonProps}>
+      <FilterIcon />
+      {label && (
+        <FilterText active={active} color={color} mobile={mobile}>
+          {label}
+        </FilterText>
+      )}
+    </FilterButtonRoot>
+  )
+}
