5e52fbb2125328f8469460d8719def6303d39d17
GuillaumeSarfati <guillaume.sarfati+github@gmail.com>
2020-12-17 03:20:14 +0100

    reviewer comments

--- docs/stories/alert.stories.js
+++ docs/stories/alert.stories.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import styled from 'react'
 import {storiesOf} from '@storybook/react'
-import {palette, Icon, VslIcon, CheckIcon, WarningIcon, ForbiddenIcon, QuestionIcon, Typography, Alert} from '@ambler/andive'
+import {Icon, VslIcon, CheckIcon, WarningIcon, ForbiddenIcon, QuestionIcon, Typography, Alert} from '@ambler/andive'
 
 import Showcase from './showcase'
 
@@ -47,42 +46,42 @@ storiesOf('NEXT|Alert', module)
           <>
         <Showcase legend="default">
           <Alert>
-            <Icon iconSize={24} icon={<WarningIcon/>}>
+            <Icon inline icon={<WarningIcon/>}>
               <Typography.Body3>Default</Typography.Body3>
             </Icon>
           </Alert>
         </Showcase>
           <Showcase legend="success">
           <Alert variant="success">
-            <Icon iconSize={24} icon={<CheckIcon/>}>
+            <Icon inline icon={<CheckIcon/>}>
               <Typography.Body3>Success</Typography.Body3>
             </Icon>
           </Alert>
           </Showcase>
           <Showcase legend="error">
           <Alert variant="error">
-            <Icon iconSize={24} icon={<ForbiddenIcon/>}>
+            <Icon inline icon={<ForbiddenIcon/>}>
               <Typography.Body3>Error</Typography.Body3>
             </Icon>
           </Alert>
           </Showcase>
           <Showcase legend="warning">
           <Alert variant="warning">
-            <Icon iconSize={24} icon={<WarningIcon/>}>
+            <Icon inline icon={<WarningIcon/>}>
               <Typography.Body3>Warning</Typography.Body3>
             </Icon>
           </Alert>
           </Showcase>
           <Showcase legend="info">
           <Alert variant="info">
-            <Icon iconSize={24} icon={<QuestionIcon/>}>
+            <Icon inline icon={<QuestionIcon/>}>
               <Typography.Body3>Info</Typography.Body3>
             </Icon>
           </Alert>
           </Showcase>
           <Showcase legend="disabled">
             <Alert variant="disabled">
-              <Icon iconSize={24} icon={<VslIcon />}>
+              <Icon inline icon={<VslIcon />}>
                 <Typography.Body3>Disabled</Typography.Body3>
               </Icon>
             </Alert>
