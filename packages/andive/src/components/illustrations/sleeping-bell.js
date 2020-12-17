79b35973317705019afec8dc677be633a5a907ac
Sébastien LE MOIGNE <sebastien.lemoigne@ambler.fr>
2020-12-17 17:09:01 +0100

    feat(illustration): add new illustration (#68)

---
+++ packages/andive/src/components/illustrations/sleeping-bell.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import SleepingBell from '../../../img/illustrations/v1/sleeping-bell.svg'
+
+export default function Illustration(props) {
+  return <SleepingBell {...props} />
+}
