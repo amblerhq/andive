ee7119dafc29b0cd64ce813f93844a04795ffd7f
GuillaumeSarfati <guillaume.sarfati+github@gmail.com>
2021-12-29 10:41:45 +0100

    fix

--- packages/andive/src/components/svg-icon.js
+++ packages/andive/src/components/svg-icon.js
@@ -8,8 +8,8 @@ const SvgRoot = styled.div`
   color: ${props => props.color};
   width: ${getWidth}px;
   height: ${getWidth}px;
-  min-width: 24px;
-  min-height: 24px;
+  min-width: ${getWidth}px;
+  min-height: ${getWidth}px;
 `
 
 const IconRoot = styled(({size, circleColor, circle, ...props}) => <SvgRoot size={size} circle={circle} {...props} />)`
