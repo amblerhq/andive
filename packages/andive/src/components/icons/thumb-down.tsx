952397d799ac3c098ae818e120c7c5a1d1dfe1d6
Luca <luca.merzetti@ambler.fr>
2022-04-15 14:54:06 +0200

    feat(icons): add thumb up down icons (#132)

---
+++ packages/andive/src/components/icons/thumb-down.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import ThumbDown from '../../../img/icons/thumb-down.svg'
+import SvgIcon from '../svg-icon'
+import {palette} from '../..'
+
+const DEFAULT_SIZE = 24
+
+const ThumbDownIcon = ({color = palette.mediumBerryBlue, size = DEFAULT_SIZE, ...rest}) => {
+  return (
+    <SvgIcon color={color} size={size} {...rest}>
+      <ThumbDown />
+    </SvgIcon>
+  )
+}
+
+ThumbDownIcon.size = DEFAULT_SIZE
+
+export default ThumbDownIcon
+export const ExportSvg = ThumbDown
---
+++ packages/andive/src/components/icons/thumb-up.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import ThumbUp from '../../../img/icons/thumb-up.svg'
+import SvgIcon from '../svg-icon'
+import {palette} from '../..'
+
+const DEFAULT_SIZE = 24
+
+const ThumbUpIcon = ({color = palette.mediumBerryBlue, size = DEFAULT_SIZE, ...rest}) => {
+  return (
+    <SvgIcon color={color} size={size} {...rest}>
+      <ThumbUp />
+    </SvgIcon>
+  )
+}
+
+ThumbUpIcon.size = DEFAULT_SIZE
+
+export default ThumbUpIcon
+export const ExportSvg = ThumbUp
