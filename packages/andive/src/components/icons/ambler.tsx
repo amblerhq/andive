b93608efdd112af0cf20e25115ed02436ef6f7b1
Sebastien LE MOIGNE <sebastien.lemoigne@ambler.fr>
2022-12-22 18:03:05 +0100

    fix(icons): fix ambler svg icon

--- packages/andive/src/components/icons/ambler.tsx
+++ packages/andive/src/components/icons/ambler.tsx
@@ -4,9 +4,9 @@ import Ambler from '../../../img/icons/ambler.svg'
 import SvgIcon from '../svg-icon'
 import {palette} from '../..'
 
-const DEFAULT_SIZE = 27
+const DEFAULT_SIZE = 24
 
-const AmblerIcon = ({color = palette.white, size = DEFAULT_SIZE, ...rest}) => {
+const AmblerIcon = ({color = palette.lightBeetrootPurple, size = DEFAULT_SIZE, ...rest}) => {
   return (
     <SvgIcon color={color} size={size} {...rest}>
       <Ambler />
