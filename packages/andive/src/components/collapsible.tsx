4a04fd100c9d3166d3b196c1f9828a7f41d8a131
Maxime <maxime.vast@ambler.fr>
2023-11-20 14:11:58 +0100

    Revert "fix(collapsible): fix collapsible trigger alignement"
    This reverts commit ecfbf313346337bb69bc439bb2179d293a3e291c.

--- packages/andive/src/components/collapsible.tsx
+++ packages/andive/src/components/collapsible.tsx
@@ -28,7 +28,7 @@ const StyledTriggerWrapper = styled.div`
   display: flex;
   flex-flow: row nowrap;
   justify-content: space-between;
-  align-items: center;
+  align-items: flex-start;
 `
 
 const StyledIconWrapper = styled.div`
